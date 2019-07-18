1306ac75432c244583f95b5ee631c909c75a438b
modugulapushpitha <pushpithamodugula@gmail.com>
2019-07-18 13:54:12 +0530

    KeepNote Step-1 with Annotations

---
+++ src/main/java/com/stackroute/keepnote/WebInitalizer.java
@@ -0,0 +1,22 @@
+package com.stackroute.keepnote;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitalizer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{ViewResolverConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
